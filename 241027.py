7d0e554d884f20f915de1937214dd65e3e71035a
Rohyoohyun <unternehmen02191@gmail.com>
2024-10-17 13:44:13 +0900

    리스트

---
+++ 241027.py
@@ -0,0 +1,11 @@
+seoul = 9876 
+incheon = 2954 
+busan=3441 
+daejun=2391
+
+place = [seoul, incheon, busan, daejun]
+
+# 인구가 가장 많은 도시, 가장 적은 도시, 네 도시 인구의 평균을 출력하는 프로그램을 작성해 봅시다.
+print(max(place))
+print(min(place))
+print(sum(place)//len(place))
